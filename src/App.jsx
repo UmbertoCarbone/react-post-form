75ae35626ca0da08933b66943161c9c5d7d472d2
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-07-23 15:46:47 +0200

    add input "name"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-
+import { useState } from "react"
 function App() {
-  
+
+  const [name, setName] = useState("")
+
+  function handleName(e) {
+    setName(e.target.value)
+  }
 
   return (
     <>
-  
+      <div className="container">
+        {/* input nome + funzione handleName */}
+        <div
+          className="card p-3 mx-auto mt-5"
+          style={{ maxWidth: "400px" }}>
+          <div className="mb-3">
+            <label className="form-label">Name</label>
+            <input type="text" className="form-control" placeholder="Type your name" value={name} onChange={handleName} />
+          </div>
+        </div>
+      </div>
+      
     </>
   )
 }
 
 export default App
+
+
+{/*  <div className="mb-3">
+          <label className="form-label">Titolo</label>
+          <input type="text" className="form-control" required>
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Testo</label>
+          <textarea className="form-control" required></textarea>
+        </div>
+        <div className="form-check mb-3">
+          <input className="form-check-input" type="checkbox" id="publicCheck">
+            <label className="form-check-label" for="publicCheck">
+              Pubblico
+            </label>
+        </div>
+        <button type="submit" className="btn btn-primary w-100">
+          Invia
+        </button> */}
