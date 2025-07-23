84c98d0a6142869293f5ac59d4c24b7a73e8adff
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-07-23 17:06:48 +0200

    modifica la struttura del modulo e corregge il segnaposto del titolo

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,16 @@ function App() {
   const [author, setAuthor] = useState("")
   const [title, setTitle] = useState("")
   const [body, setBody] = useState("")
+
+  //API
+  const url = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts"
+
+  //fetch
+  /*   useEffect(() => {
+     fetch(url)
+       .then((res) => res.json())
+       .then("");
+   }, []);  */
   //
   function handleAuthor(e) {
     setAuthor(e.target.value)
@@ -21,7 +31,7 @@ function App() {
   return (
     <>
       <div className="container">
-        <div className="card p-3 mx-auto mt-5" style={{ maxWidth: "400px" }}>
+        <form className="card p-3 mx-auto mt-5" style={{ maxWidth: "400px" }}>
           {/* input nome + funzione handleName */}
           <div className="mb-3">
             <label className="form-label">Name</label>
@@ -30,18 +40,22 @@ function App() {
           {/* input Title + funziona handleTitle */}
           <div className="mb-3">
             <label className="form-label">Title</label>
-            <input type="text" className="form-control" placeholder="Type your tile" value={title} onChange={handleTitle} />
+            <input type="text" className="form-control" placeholder="Type your title" value={title} onChange={handleTitle} />
           </div>
           {/* body + button + funzione handleBody */}
           <div className="mb-3">
             <label className="form-label">Post</label>
             <textarea className="form-control" placeholder="Type your post" value={body} onChange={handleBody} />
           </div>
+          <div className="mb-3 form-check">
+            <label className="form-check-label">Published</label>
+            <input type="checkbox" className="form-check-input" />
+          </div>
           <button type="submit" className="btn btn-primary w-100">
             Invia
           </button>
           {/* fine container + card */}
-        </div>
+        </form>
       </div>
     </>
   )
